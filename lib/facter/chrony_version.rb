8a662064f2f666b9e7ac1d97de42bdf921ba0efe
Daniele Palumbo <daniele@retaggio.net>
2018-12-06 00:21:47 +0100

    Fixed chrony_version for "non rhel"

--- lib/facter/chrony_version.rb
+++ lib/facter/chrony_version.rb
@@ -1,10 +1,18 @@
+require 'puppet'
 Facter.add('chrony_version') do
     confine :kernel => 'Linux'
-    setcode do
-        if Facter::Core::Execution.which("chronyd")
-            Facter::Core::Execution.execute("/bin/rpm -q --queryformat '%{VERSION}' chrony")
-        else
-            nil
-        end
+  setcode do
+    if Facter::Util::Resolution.which('chronyc')
+      chrony_version = Facter::Util::Resolution.exec('chronyc -v 2>&1')
+      # eg chronyc (chrony) version 3.0 (+READLINE +IPV6 -DEBUG)
+      %r{^chronyc (chrony) version ([\w\.]+)}.match(sudoversion)[1]
+    elsif Facter::Util::Resolution.which('rpm')
+      Facter::Util::Resolution.exec('rpm -q --queryformat \'%{VERSION}\' chrony')
+    elsif Facter::Util::Resolution.which('dpkg-query')
+      Facter::Util::Resolution.exec('dpkg-query -W -f=\'${Version}\n\' chrony')
+    else
+      nil
     end
+  end
 end
+
