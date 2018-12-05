d2eedee68a7017334e051508a4c6bc4629dba9f1
Daniele Palumbo <daniele@retaggio.net>
2018-12-06 00:24:45 +0100

    Fixed logic for version retrieval

--- lib/facter/chrony_version.rb
+++ lib/facter/chrony_version.rb
@@ -5,7 +5,7 @@ Facter.add('chrony_version') do
     if Facter::Util::Resolution.which('chronyc')
       chrony_version = Facter::Util::Resolution.exec('chronyc -v 2>&1')
       # eg chronyc (chrony) version 3.0 (+READLINE +IPV6 -DEBUG)
-      %r{^chronyc (chrony) version ([\w\.]+)}.match(sudoversion)[1]
+      %r{^chronyc \(chrony\) version ([\w\.]+)}.match(chrony_version)[1]
     elsif Facter::Util::Resolution.which('rpm')
       Facter::Util::Resolution.exec('rpm -q --queryformat \'%{VERSION}\' chrony')
     elsif Facter::Util::Resolution.which('dpkg-query')
