245ca0698dceb32d2cade825c13fcdf35e1cb624
Daniele Palumbo <daniele@retaggio.net>
2020-06-23 17:05:40 +0200

    Added OS package scope, to disallow wrong pkg cmd
    In example, if Debian has rpm command installed, this will cause no harm.

--- lib/facter/chrony_version.rb
+++ lib/facter/chrony_version.rb
@@ -6,9 +6,9 @@ Facter.add('chrony_version') do
       chrony_version = Facter::Util::Resolution.exec('chronyc -v 2>&1')
       # eg chronyc (chrony) version 3.0 (+READLINE +IPV6 -DEBUG)
       %r{^chronyc \(chrony\) version ([\w\.]+)}.match(chrony_version)[1]
-    elsif Facter::Util::Resolution.which('rpm') and :osfamily = 'redhat'
+    elsif Facter::Util::Resolution.which('rpm') and Facter.value(:osfamily) == 'redhat'
       Facter::Util::Resolution.exec('rpm -q --queryformat \'%{VERSION}\' chrony')
-    elsif Facter::Util::Resolution.which('dpkg-query') and :osfamily = 'debian'
+    elsif Facter::Util::Resolution.which('dpkg-query') and Facter.value(:osfamily) == 'debian'
       Facter::Util::Resolution.exec('dpkg-query -W -f=\'${Version}\n\' chrony')
     else
       nil
