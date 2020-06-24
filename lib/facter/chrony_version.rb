e731bd59dbea042879b614323dfed2a549987b7c
Daniele Palumbo <daniele@retaggio.net>
2020-06-24 23:08:01 +0200

    facter chrony version and bugfix (#3)
    * Fixed data yaml and Debian supported distributions
    
    data directory was containing in yaml file config in place of config_file
    Debian support has been updated to oldstable and stable (testing not tested)
    
    * Fixed chrony_version for "non rhel"
    
    * Fixed logic for version retrieval
    
    * Added OS package scope, to disallow wrong pkg cmd
    
    In example, if Debian has rpm command installed, this will cause no harm.
    
    * Added OS package scope, to disallow wrong pkg cmd
    
    In example, if Debian has rpm command installed, this will cause no harm.

--- lib/facter/chrony_version.rb
+++ lib/facter/chrony_version.rb
@@ -1,10 +1,18 @@
+require 'puppet'
 Facter.add('chrony_version') do
-    confine :kernel => 'Linux'
-    setcode do
-        if Facter::Core::Execution.which("chronyd")
-            Facter::Core::Execution.execute("/bin/rpm -q --queryformat '%{VERSION}' chrony")
-        else
-            nil
-        end
+  confine :kernel => 'Linux'
+  setcode do
+    if Facter::Util::Resolution.which('chronyc')
+      chrony_version = Facter::Util::Resolution.exec('chronyc -v 2>&1')
+      # eg chronyc (chrony) version 3.0 (+READLINE +IPV6 -DEBUG)
+      %r{^chronyc \(chrony\) version ([\w\.]+)}.match(chrony_version)[1]
+    elsif Facter::Util::Resolution.which('rpm') and Facter.value(:osfamily) == 'redhat'
+      Facter::Util::Resolution.exec('rpm -q --queryformat \'%{VERSION}\' chrony')
+    elsif Facter::Util::Resolution.which('dpkg-query') and Facter.value(:osfamily) == 'debian'
+      Facter::Util::Resolution.exec('dpkg-query -W -f=\'${Version}\n\' chrony')
+    else
+      nil
     end
+  end
 end
+
